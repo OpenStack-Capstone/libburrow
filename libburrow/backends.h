b0406813e9ada52f76c6f92b8487ec1ae9291180
Tony Wooster <twooster@gmail.com>
2011-08-02 17:49:29 -0700

    Updated licenses

--- libburrow/backends.h
+++ libburrow/backends.h
@@ -1,3 +1,26 @@
+/*
+ * libburrow -- Burrow Client Library
+ *
+ * Copyright 2011 Tony Wooster 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file
+ * @brief Backend loader declarations
+ */
+
 #ifndef __BURROW_BACKENDS_H
 #define __BURROW_BACKENDS_H
 
